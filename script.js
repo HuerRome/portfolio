6a19a4486d3bb997758c2ae5f62f905273431f8b
HuerRome <4n.rooo@gmail.com>
2022-03-25 16:37:17 -0300

    code cards

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ button.addEventListener('click',()=>{
     menudes.classList.toggle("active");
 })
 
+//texto efecto
+let tittle=document.querySelector(".container > .introduction > div:first-child");
+//let boton=document.querySelector(".container > .introduction > div:first-child div");
+window.addEventListener("load",()=>{
+    tittle.style.marginTop= "9vh";
+    tittle.style.opacity="1";
+    //boton.style.opacity="1";
+})
+
+
+
 /*--------------------*/
 console.log(window.innerWidth);
 console.log(window.innerHeight)
