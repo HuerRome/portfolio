a99c71f9844efc5e7767d4ff522cc02976963b57
HuerRome <4n.rooo@gmail.com>
2022-03-26 16:54:38 -0300

    change the sizes

--- script.js
+++ script.js
@@ -18,25 +18,27 @@ let tittle=document.querySelector(".container > .introduction > div:first-child"
 window.addEventListener("load",()=>{
     tittle.style.marginTop= "9vh";
     tittle.style.opacity="1";
-    //boton.style.opacity="1";
 })
 
 //menu scroll
+//menu scroll
 let menu= document.querySelector(".links");
-let ubicacionPrincipal = window.pageYOffset;
-window.onscroll = function(){
-  let Desplazamiento_Actual = window.pageYOffset;
+window.addEventListener("scroll",() => {
+  menu.classList.toggle("up", window.scrollY>0)
+})
 
-  ubicacionPrincipal >= Desplazamiento_Actual ? menu.style.top = '0' : menu.style.top = '-79px';
-}
+let menu1= document.querySelector(".links-cel");
+window.addEventListener("scroll",() => {
+  menu1.classList.toggle("up", window.scrollY>0)
+})
 
-let menu2= document.querySelector(".links-cel");
+/*let menu2= document.querySelector(".links-cel");
 let ubicacionPrincipal2 = window.pageYOffset;
 window.onscroll = function(){
   let Desplazamiento_Actual = window.pageYOffset;
 
   ubicacionPrincipal2 >= Desplazamiento_Actual ? menu2.style.top = '0' : menu2.style.top = '-79px';
-}
+}*/
 
 /*--------------------*/
 console.log(window.innerWidth);
