8b79028583633228524d94a6df89e6f2d6d30c0c
HuerRome <4n.rooo@gmail.com>
2022-03-26 12:25:03 -0300

    menu scroll

--- script.js
+++ script.js
@@ -21,7 +21,22 @@ window.addEventListener("load",()=>{
     //boton.style.opacity="1";
 })
 
+//menu scroll
+let menu= document.querySelector(".links");
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function(){
+  let Desplazamiento_Actual = window.pageYOffset;
 
+  ubicacionPrincipal >= Desplazamiento_Actual ? menu.style.top = '0' : menu.style.top = '-79px';
+}
+
+let menu2= document.querySelector(".links-cel");
+let ubicacionPrincipal2 = window.pageYOffset;
+window.onscroll = function(){
+  let Desplazamiento_Actual = window.pageYOffset;
+
+  ubicacionPrincipal2 >= Desplazamiento_Actual ? menu2.style.top = '0' : menu2.style.top = '-79px';
+}
 
 /*--------------------*/
 console.log(window.innerWidth);
