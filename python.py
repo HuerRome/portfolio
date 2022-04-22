f903d5a0cdf7aa3efe238fe6f1b70968c81c13b4
HuerRome <4n.rooo@gmail.com>
2022-04-22 02:57:02 -0300

    header

--- python.py
+++ python.py
@@ -1,57 +1,23 @@
-'''
-Captura la frase del teclado y luego transformala en minuscula, mostrar ambas cadenas.
-por ejemplo de la prueba las siguientes cadenas: SI , COMPRENDO LO QUE DICES.
-'''
 
 
-'''
-1.- capturando frase input se utiliza para pedir un dato
-2.- Ahora mostramos ambas cadenas tanto en minuscula como en mayuscula
-3.- Ahora mostramos (imprimos por pantalla) el resultado
-'''
-'''
-Frase = input('Ingrese una frase por favor: ')
+from itertools import count
+from operator import le, truediv
 
-FraseMayuscula = Frase.upper()
-FraseMinuscula = Frase.lower()
+from math import sin, cos
+
+lista= []
+
+for i in range(91):
+    var = sin(2*i) 
+    lista.append(var)
+
+print(len(lista))
+
+contador=0
+for x in lista:
+    print('numero: ',contador,'--->',lista[lista.index(x)], 'se repite', lista.count(lista[lista.index(x)]))
+    contador+=1
 
 
-print('La frase en mayuscula es: ', FraseMayuscula)
-print('La frase en minuscula es: ', FraseMinuscula)
-'''
 
 
-#de la siguiente cadena indique:
-#"Se suspende los partidos de fútbol por 10 días, en vez de fútbol haremos gimnacia funcional en casa"
-#1. Cantidad de letras de la cadena 
-#2. Cantidad de veces que se repite la letra "e"
-#3. Cantidad de veces que se repite la palabra fútbol
-#4. Reemplazar fútbol por voley
-
-Cadena = 'Se suspende los partidos de fútbol por 10 días, en vez de fútbol haremos gimnacia funcional en casa'
-ListaPalabras = Cadena.split()
-
-#Punto 1. 
-for palabra in ListaPalabras:
-    ListaLetras= list(palabra)
-
-    print(f'Palabra numero: {ListaPalabras.index(palabra)+1}')
-    print(f'--> Cantidad de letras: {len(ListaLetras)}')
-
-#Punto 2.
-contador = 0
-for palabra in ListaPalabras:
-    for letra in palabra:
-        if(letra == 'e' or letra == 'E'):
-            contador+=1
-
-print('La letra e se repite', contador,'veces')
-
-#Punto 3.
-futbol = ListaPalabras.count('fútbol')
-print(f'fútbol se repite {futbol} veces')
-
-#Punto 4.
-Cambio = Cadena.replace('fútbol','voley')
-
-resultado = print(Cambio)
